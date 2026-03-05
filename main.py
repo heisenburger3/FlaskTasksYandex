6c1ba4d446b1f355748789806179d2b71bceda3c
Rinat <rinat.rinat.1302@gmail.com>
2026-03-05 17:42:34 +0300

    2 задача

--- main.py
+++ main.py
@@ -14,5 +14,23 @@ def index():
     return 'И на Марсе будут яблони цвести!'
 
 
+@app.route('/promotion')
+def promotion():
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Реклама</title>
+                      </head>
+                      <body>
+                        <h1>Человечество вырастает из детства.</h1>
+                        <h1>Человечеству мала одна планета.</h1>
+                        <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
+                        <h1>И начнем с Марса!</h1>
+                        <h1>Присоединяйся!</h1>
+                      </body>
+                    </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
