4022760dfbace267687acab3e667cd79fc35fec2
Rinat <rinat.rinat.1302@gmail.com>
2026-03-05 17:52:07 +0300

    4 задача

--- main.py
+++ main.py
@@ -48,5 +48,40 @@ def image_mars():
                     </html>"""
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="/static/css/style.css" />
+                        <link rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                        crossorigin="anonymous">
+                        <title>Колонизация</title>
+                        </head>
+                        <body>
+                            <h1>Жди нас, Марс!</h1>
+                            <img src="/static/img/mars.jpg">
+                            <div class="alert alert-secondary" role="alert">
+                             Человечество вырастает из детства.
+                              </div>
+                                <div class="alert alert-success" role="alert">
+                                  Человечеству мала одна планета.
+                                    </div>
+                                      <div class="alert alert-secondary" role="alert">
+                                        Мы сделаем обитаемыми безжизненные пока планеты.
+                                          </div>
+                                            <div class="alert alert-warning" role="alert">
+                                              И начнем с Марса!
+                                                </div>
+                                                  <div class="alert alert-danger" role="alert">
+                                                    Присоединяйся!
+                                                      </div>
+                            </body>
+                        </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
