5169b9ab08bb798b54e2e79d56cd4514f4996806
Rinat <rinat.rinat.1302@gmail.com>
2026-03-11 22:38:55 +0300

    6 задача 2.0

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['POST', 'GET'])
+def main_page():
+    return render_template('base.html', title='Заготовка')
+
 @app.route('/index/<string:title>')
 def index(title):
     return render_template('base.html', title=title)
